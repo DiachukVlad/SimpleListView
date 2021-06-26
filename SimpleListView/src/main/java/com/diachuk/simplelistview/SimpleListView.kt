81293dd9300b5021ab1e7f87caa50ae6ad828323
vlad <vladiachuk@gmail.com>
2021-06-26 20:47:28 +0200

    initial commit

---
+++ SimpleListView/src/main/java/com/diachuk/simplelistview/SimpleListView.kt
@@ -0,0 +1,17 @@
+package com.diachuk.simplelistview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+class SimpleListView(context: Context, attrs: AttributeSet? = null) : RecyclerView(context, attrs) {
+	fun <DataType, BindingType : ViewBinding> createWithData(
+		data: List<DataType>,
+		createBinding: (LayoutInflater) -> BindingType,
+		bind: BindingType.(pos: Int) -> Unit
+	) {
+		adapter = SimpleAdapter(data, createBinding, bind)
+	}
+}
